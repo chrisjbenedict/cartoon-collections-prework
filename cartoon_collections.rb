77040c4ca26d56e267dd514522562c651921307b
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 22:03:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.collect do |call|
-    arr = call.split('')
-    arr[0].upcase
-    loud_call = arr.join('') + "!"
-  end
+  planeteer_calls.map! {|call| call.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
